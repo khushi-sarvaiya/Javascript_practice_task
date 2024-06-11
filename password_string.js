7ee0eb3ca5a170de3a78c90bafa8f4727fa9684d
khushi <khushisarvaiya410@gmail.com>
2024-06-11 16:18:39 +0530

    final commit

--- password_string.js
+++ password_string.js
@@ -1,12 +1,14 @@
 const fs = require('fs')
-let new_arr;
+let new_arr=[];
+
 fs.readFile('password.txt', (err, data) => {
   if (err) throw err;   
     new_data = (data.toString());
     arr=(new_data.split("\n"));
     for (let i = 0; i < arr.length; i++){
-       new_arr=arr[i].split(":");
+       new_arr[i]=arr[i].split(":");
     }    
- console.log(new_arr);
+   console.log(new_arr);
+   
 });
 
